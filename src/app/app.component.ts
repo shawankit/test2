b77079deb5c673713f3981775791495684507f58
Ankit Shaw <ankit.shaw@keross.com>
2021-05-09 12:49:31 +0530

    Add all my files

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,8 @@
-import { Component } from '@angular/core';
+
+
+import { Component, OnInit, HostListener, Inject } from '@angular/core';  
+import { trigger, state, transition, style, animate } from '@angular/animations';  
+import { DOCUMENT } from '@angular/common';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +11,16 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'openBankUI';
+  constructor(@Inject(DOCUMENT) document) { }
+
+  @HostListener('window:scroll', ['$event'])
+  onWindowScroll(e) {
+     if (window.pageYOffset > 0) {
+       let element = document.getElementById('header');
+       element.classList.add('sticky');
+     } else {
+      let element = document.getElementById('header');
+        element.classList.remove('sticky'); 
+     }
+  }
 }
